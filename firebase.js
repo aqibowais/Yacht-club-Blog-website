c87cfc71b0a5c1e0c46ff8cce82d04d2fe306c5c
Aqib Owais <owaisharoon60@gmail.com>
2024-08-17 18:47:39 +0500

    Yacht blog website UI design

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZKdNbGO_yzKgbulvkVpX4fEZkhIuL-DM",
+  authDomain: "yacht-club-ac507.firebaseapp.com",
+  projectId: "yacht-club-ac507",
+  storageBucket: "yacht-club-ac507.appspot.com",
+  messagingSenderId: "634974582483",
+  appId: "1:634974582483:web:c3dc1f694362f2dff5c626",
+  measurementId: "G-JR5RR1SEFN",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
